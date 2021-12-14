354f2f0749a5cbc4bcb2f365c0c50d33ce3029f8
randreast <rizky.andreas93@gmail.com>
2021-12-15 00:08:54 +0700

    add library to read page body and write to csv

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/randreast/ecommerce-scraper
 
 go 1.17
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/joho/godotenv"
 )
 
+func scraper(csvWriter *csv.Writer, limit int) {
+	res, err := http.Get("https://www.tokopedia.com/p/handphone-tablet/handphone?ob=5&page=1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title
+		if i < limit {
+			data := []string{
+				s.Find(".css-1bjwylw").Text(),
+				s.Find(".css-o5uqvq").Text(),
+				s.Find(".css-1kr22w3").Text(),
+			}
+
+			csvWriter.Write(data)
+		}
+	})
+
+	csvWriter.Flush()
+}
+
 func main() {
 	godotenv.Load()
-	limit := os.Getenv("LIMIT")
-	fmt.Println(limit)
+	limit, err := strconv.Atoi(os.Getenv("LIMIT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvFile, err := os.Create("product.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvWriter := csv.NewWriter(csvFile)
+
+	scraper(csvWriter, limit)
+
+	csvFile.Close()
 }
